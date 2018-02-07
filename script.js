1d111b1c459dc5653c267889c190ce8b76c82e15
Abigayle Franklin <abifranklin@gmail.com>
2018-02-07 15:59:59 -0600

    JS functional in JSBin

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-$(document).ready(function() {
-$(.container).css("border-radius", "50%");
-
+$(document).ready(function () {
+var listOfAnswers = ["It is certain", "It is decidedly so", "Without a doubt",
+  "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely",
+  "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again",
+  "Ask again later", "Better not tell you now", "Cannot predict now",
+  "Concentrate and ask again", "Don't count on it", "My reply is no",
+  "My sources say no", "Outlook not so good", "Very doubtful"];
+$("#shake").click(function(){
+  var randomNumber = Math.random();
+  var randomNumberForListOfAnswers = randomNumber * listOfAnswers.length;
+  var randomIndex = Math.floor(randomNumberForListOfAnswers);
+  var answerText = listOfAnswers[randomIndex];
+  $("#answer").text(answerText);
+  });
 });
